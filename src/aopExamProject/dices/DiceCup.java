dcf0e89e2c3f879ea27f8687bd164b5618236907
AnjaWurli <anja.wurlitzer.coding@gmail.com>
2026-09-04 17:06:07 +0200

    add dice roll counter

--- src/aopExamProject/dices/DiceCup.java
+++ src/aopExamProject/dices/DiceCup.java
@@ -6,11 +6,15 @@ import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class DiceCup { 
 	private List<DiceRollListener> listeners = new ArrayList<>();
 	
 	private List<Dice> dices;
+	private int count = 3;
+	private JLabel counterLabel;
+	JButton button = new JButton("würfeln");
 	protected final JFrame cupUI; // change to JPanel when merged (?)
 	
 	public DiceCup() {
@@ -19,8 +23,10 @@ public class DiceCup {
 		cupUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		cupUI.setLayout(new FlowLayout());
 		
-		JButton button = new JButton("würfeln");
 		cupUI.add(button);
+		
+		counterLabel = new JLabel(String.format("übrige Würfe: %d", count));
+		cupUI.add(counterLabel);
 		
 		dices = new ArrayList<>();
 		for (int i = 0; i < 5; i++) {
@@ -47,9 +53,18 @@ public class DiceCup {
 	}
 	
 	public void rollDices() {
+		count--;
+		counterLabel.setText(String.format("übrige Würfe: %d", count));
 		for(Dice dice : dices) {
 			dice.rollDice();
 		}
+		
+		if(count<=0) {
+			button.setEnabled(false);
+			return;
+		}
+	}
+	
 	public void addDiceRollListener(DiceRollListener listener) {
 		listeners.add(listener);
 	}
