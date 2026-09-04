451b566f4b11dd322828a397bab911bb8b6a2c1a
Dennis S. <sharkwithhat@gmail.com>
2026-09-04 18:37:05 +0200

    Label und Methode erstellt um den aktuellen Spieler anzuzeigen

--- src/aopExamProject/Spielemechanik/Player.java
+++ src/aopExamProject/Spielemechanik/Player.java
@@ -9,7 +9,7 @@ public class Player {
 	
 	public Player(String name, int id) 
 	{
-		this.name = name;
+		this.setName(name);
 		this.id = id;
 		//this.score = new Scoreboard();
 	}
@@ -20,5 +20,13 @@ public class Player {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
