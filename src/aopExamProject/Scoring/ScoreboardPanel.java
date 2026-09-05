d6f9d1ee976b6bed06b3cfe87807ed6a39db5224
AnjaWurli <anja.wurlitzer.coding@gmail.com>
2026-09-05 12:41:42 +0200

    add testmain and JFrame to run the code

--- src/aopExamProject/Scoring/ScoreboardPanel.java
+++ src/aopExamProject/Scoring/ScoreboardPanel.java
@@ -1,26 +1,36 @@
 package aopExamProject.Scoring;
 
 import java.awt.GridLayout;
+
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ScoreboardPanel extends JPanel {
+	protected final JFrame scoreUI;
+	
 	private Scoreboard board;
 	private JLabel[] text;
 	
 	public ScoreboardPanel(Scoreboard scoreboard) {
+		scoreUI = new JFrame("Würfelbecher");
+		scoreUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		this.board = scoreboard;
 		this.text = new JLabel[13];
 		//13 Zeilen, zwei spalten erstmal
-		setLayout(new GridLayout(13, 2));
+		scoreUI.setLayout(new GridLayout(13, 2));
 		String[] name = {
 				"Einser", "Zweier", "Dreier", "Vierer", "FÜnfer", "Sechser", "Dreierpasch", "Viererpasch", "Full House", "Kleine Straße", "Große Straße", "Kniffel", "Chance"
 		};
 		for (int i = 0; i < 13; i++) {
-			add(new JLabel(name[i])); //links
+			scoreUI.add(new JLabel(name[i])); //links
 			text[i] = new JLabel("-"); //rechts
-			add(text[i]);
+			scoreUI.add(text[i]);
 		}
+		
+		scoreUI.pack();
+		scoreUI.setVisible(true);
 	}
 	public void testCompat() {
 		assert true;
